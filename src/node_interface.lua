d708a92855c5eaf24e59263b93dc52d98485e469
Demeter Áron <demeter.aron0614@gmail.com>
2023-05-23 23:55:19 +0200

    yesn't

---
+++ src/node_interface.lua
@@ -0,0 +1,7 @@
+---@author: Demeter Áron 2023-05-23 22:21:49
+---@version: 1.0.0
+---@description: The node interface classes
+
+-- Path: src/node_interface.lua
+
+---------------------------------------------
